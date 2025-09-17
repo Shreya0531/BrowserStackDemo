75d52993a6fc526fb95aa895b96b580cbd1682b0
Akash Krishna <krishna@Akashs-MacBook-Air.local>
2025-09-17 12:52:54 +0530

    Removed flow calls from test

--- src/main/java/org/browserStack/flows/AddToCartFlow.java
+++ src/main/java/org/browserStack/flows/AddToCartFlow.java
@@ -3,26 +3,21 @@ package org.browserStack.flows;
 import org.browserStack.pages.HomePage;
 import org.openqa.selenium.WebDriver;
 
-
 import java.util.List;
 
 public class AddToCartFlow {
     HomePage homePage;
 
-   public AddToCartFlow(WebDriver driver){
+    public AddToCartFlow(WebDriver driver) {
         this.homePage = new HomePage(driver);
     }
 
-    public void addRandomProductsToCart(int count){
-       homePage.addMultipleRandomProductsToCart(count);
+    public void proceedToCheckout() {
+        homePage.clickOnCheckoutButton();
     }
 
-    public void proceedToCheckout(){
-       homePage.clickOnCheckoutbutton();
-    }
-
-    public List<String> getAllProductsList(){
-       return homePage.getAllProductNames();
+    public List<String> getAllProductsList() {
+        return homePage.getAllProductsNamesInString();
     }
 
 }
